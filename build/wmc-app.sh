e7f6524a9be2ba30e688b86ae281701b5e44b72d
Nguyen Phu Quang <nguyenphuquang@gmail.com>
2017-10-04 13:43:05 +0700

    Fix

--- build/wmc-app.sh
+++ build/wmc-app.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # https://github.com/brettwooldridge/HikariCP#configuration-knobs-baby
 # https://stackoverflow.com/questions/38688489/hikaripool-1-connection-is-not-available-request-timed-out-after
-REDIS_CONNECTIONS=session:6379 /usr/bin/java -jar wmc-app.war
+REDIS_CONNECTIONS=session.host:6379 /usr/bin/java -jar wmc-app.war
